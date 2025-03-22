b3c28a76cbf84d7a6ee7406b30cc3c16a07041a4
mbendana <59422970+mbendana@users.noreply.github.com>
2025-03-21 19:18:31 -0600

    Add 2of7.js file

---
+++ 2of7/2of7.js
@@ -0,0 +1,12 @@
+// Application which has asks the user the following questions:
+// What's your name?
+// How old are you?
+// What programming language are you studying?
+
+let userName = prompt("What's your name? ");
+let userAge = prompt("How old are you? ");
+let userProgLang = prompt("What programming language are you studying? ");
+
+console.log(`Hello ${userName}, you are ${userAge} years old and` +
+    ` already studyging ${userProgLang}.`);
+
